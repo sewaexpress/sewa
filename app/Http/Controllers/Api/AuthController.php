2d12c6a75ec862b36ffbc0430c8a7a3f6d71cf01
JoshiBipin <bipin.ingrails@gmail.com>
2022-12-24 19:52:26 +0545

    check

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -39,6 +39,7 @@ class AuthController extends Controller
             $customer = new Customer;
             $customer->user_id = $user->id;
              $customer->save();
+            //  asdfasdfasdf
          }
          catch(\Exception $e){
             return response()->json([
