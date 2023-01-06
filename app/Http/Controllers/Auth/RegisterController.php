1a00d3bda1aeb04dc240b5611f1a4c58396092de
JoshiBipin <bipin.ingrails@gmail.com>
2023-01-06 10:20:12 +0545

    otp 2

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -147,15 +147,15 @@ class RegisterController extends Controller
         $customer = new Customer;
         $customer->user_id = $user->id;
         $customer->save();
-        if(BusinessSetting::where('type', 'email_verification')->first()->value != 1){
-            $user->email_verified_at = date('Y-m-d H:m:s');
-            $user->save();
-            flash(__('Registration successfull.'))->success();
-        }
-        else {
+        // if(BusinessSetting::where('type', 'email_verification')->first()->value != 1){
+        //     $user->email_verified_at = date('Y-m-d H:m:s');
+        //     $user->save();
+        //     flash(__('Registration successfull.'))->success();
+        // }
+        // else {
             $user->sendCustomVerificationEmail($otp_code);
             flash(__('Registration successfull. Please verify your email.'))->success();
-        }
+        // }
         Session::put('registered',1);
         $this->guard()->login($user);
         return $this->registered($request, $user)
