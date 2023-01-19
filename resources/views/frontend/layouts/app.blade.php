29c7c0bbb3e1117f7e0de5e57f92ead688611a12
JoshiBipin <bipin.ingrails@gmail.com>
2023-01-19 23:36:34 +0545

    billing issue check

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -1488,7 +1488,58 @@ $('.address-district').on('change',function(e){
             showFrontendAlert('success', 'Copied to clipboard');
         }
     </script>
+<script type="text/javascript">
 
+    $('#dashboard-phone2').on('keypress', function(e) {
+        var $this = $(this);
+        var regex = new RegExp("^[0-9\b]+$");
+        var str = String.fromCharCode(!e.charCode ? e.which : e.charCode);
+        // for 10 digit number only
+        if ($this.val().length > 9) {
+            e.preventDefault();
+            return false;
+        }
+        if (e.charCode < 57 && e.charCode > 47) {
+            if ($this.val().length == 0) {
+                e.preventDefault();
+                return false;
+            } else {
+                return true;
+            }
+        }
+        if (regex.test(str)) {
+            return true;
+        }
+        e.preventDefault();
+        return false;
+    });
+    $('#dashboard-phone').on('keypress', function(e) {
+        var $this = $(this);
+        var regex = new RegExp("^[0-9\b]+$");
+        var str = String.fromCharCode(!e.charCode ? e.which : e.charCode);
+        // for 10 digit number only
+        if ($this.val().length > 9) {
+            e.preventDefault();
+            return false;
+        }
+        if (e.charCode < 57 && e.charCode > 47) {
+            if ($this.val().length == 0) {
+                e.preventDefault();
+                return false;
+            } else {
+                return true;
+            }
+        }
+        if (regex.test(str)) {
+            return true;
+        }
+        e.preventDefault();
+        return false;
+    });
+    function add_new_address_dashboard(){
+        $('#new-address-modal').modal('show');
+    }
+</script>
     @yield('script')
 
 </body>
