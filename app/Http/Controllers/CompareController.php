ccbbbdb1fe46a854de24cebd3c7acdaf1e8ed6e7
JoshiBipin <bipin.ingrails@gmail.com>
2023-01-22 22:57:53 +0545

    compare

--- app/Http/Controllers/CompareController.php
+++ app/Http/Controllers/CompareController.php
@@ -28,6 +28,8 @@ class CompareController extends Controller
     {
         $request->session()->forget('compare');
         $request->session()->forget('compare_category');
+        $request->session()->forget('compare_subcategory');
+        $request->session()->forget('compare_subsubcategory');
         return back();
     }
 
@@ -36,8 +38,9 @@ class CompareController extends Controller
     {
         $product = Product::find($request->id);
         if($request->session()->has('compare')){
-            if($request->session()->has('compare_category')){
-                if($request->session()->get('compare_category') == $product->category){
+            if($request->session()->has('compare_subsubcategory')){
+                // dd('31');
+                if($request->session()->get('compare_subsubcategory') == ((isset($product->subsubcategory->id))?$product->subsubcategory->id:0)){
                     $compare = $request->session()->get('compare', collect([]));
                     if(!$compare->contains($request->id)){
                         if(count($compare) == 3){
@@ -48,29 +51,83 @@ class CompareController extends Controller
                             $compare->push($request->id);
                         }
                     }
-                    // $request->session()->get('compare_category');
                 }else{
                     return 'false';
                 }
-            }
-            else{
-                $compare = $request->session()->get('compare', collect([]));
-                $request->session()->put('compare_category', $product->category);
-                if(!$compare->contains($request->id)){
-                    if(count($compare) == 3){
-                        $compare->forget(0);
-                        $compare->push($request->id);
+            }elseif($request->session()->has('compare_subcategory')){
+                if($request->session()->get('compare_subcategory') == ((isset($product->subcategory->id))?$product->subcategory->id:0)){
+                    $compare = $request->session()->get('compare', collect([]));
+                    if(!$compare->contains($request->id)){
+                        if(count($compare) == 3){
+                            $compare->forget(0);
+                            $compare->push($request->id);
+                        }
+                        else{
+                            $compare->push($request->id);
+                        }
                     }
-                    else{
-                        $compare->push($request->id);
-                    }
+                }else{
+                    return 'false';
                 }
+            }else{
+                // dd('12');
+                if($request->session()->get('compare_category') == ((isset($product->subcategory->id))?$product->subcategory->id:0)){
+                    $compare = $request->session()->get('compare', collect([]));
+                    if(!$compare->contains($request->id)){
+                        if(count($compare) == 3){
+                            $compare->forget(0);
+                            $compare->push($request->id);
+                        }
+                        else{
+                            $compare->push($request->id);
+                        }
+                    }
+                }else{
+                    return 'false';
+                }
+
             }
+
+            // if($request->session()->has('compare_category')){
+            //     if($request->session()->get('compare_category') == $product->category){
+            //         $compare = $request->session()->get('compare', collect([]));
+            //         if(!$compare->contains($request->id)){
+            //             if(count($compare) == 3){
+            //                 $compare->forget(0);
+            //                 $compare->push($request->id);
+            //             }
+            //             else{
+            //                 $compare->push($request->id);
+            //             }
+            //         }
+            //     }else{
+            //         return 'false';
+            //     }
+            // }
+            // else{
+            //     $compare = $request->session()->get('compare', collect([]));
+            //     $request->session()->put('compare_category', $product->category);
+            //     if(!$compare->contains($request->id)){
+            //         if(count($compare) == 3){
+            //             $compare->forget(0);
+            //             $compare->push($request->id);
+            //         }
+            //         else{
+            //             $compare->push($request->id);
+            //         }
+            //     }
+            // }
         }
         else{
             $compare = collect([$request->id]);
             $request->session()->put('compare', $compare);
-            $request->session()->put('compare_category', $product->category);
+            if($product->subsubcategory){
+                $request->session()->put('compare_subsubcategory', $product->subsubcategory->id);
+            }elseif($product->subcategory){
+                $request->session()->put('compare_subcategory', $product->subcategory->id);
+            }else{
+                $request->session()->put('compare_category', $product->category->id);
+            }
         }
         // flash(__('Passwords donot match'))->error();
 
