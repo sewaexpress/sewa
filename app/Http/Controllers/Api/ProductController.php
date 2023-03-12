a333337ed878ab55f3ddfcdec1458ff437d23142
JoshiBipin <bipin.ingrails@gmail.com>
2023-03-12 23:19:12 +0545

    changes

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -227,7 +227,7 @@ class ProductController extends Controller
 
     public function featured()
     {
-        $products =  filter_products(\App\Product::orderBy('id','DESC')->where('featured', 1)->where('current_stock','>',0)->with('stocks')->paginate(20));
+        $products =  filter_products(\App\Product::orderBy('id','DESC')->where('featured', 1)->where('current_stock','>',0)->with('stocks'))->paginate(20);
         return new ProductCollection($products);
         // ->get();
         // return new ProductCollection(filter_products(Product::orderBy('id','DESC')->where('featured', 1)->get()));
