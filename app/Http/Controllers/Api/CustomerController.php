7b75eeb546d36bd5abc844f2d4c439ef1d61f965
JoshiBipin <bipin.ingrails@gmail.com>
2023-03-21 23:08:25 +0545

    app coupon part

--- app/Http/Controllers/Api/CustomerController.php
+++ app/Http/Controllers/Api/CustomerController.php
@@ -4,8 +4,11 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Resources\CustomerResource;
 use App\Http\Resources\SupportTicketCollection;
+use App\Models\Cart;
 use App\Models\Coupon;
+use App\Models\CouponUsage;
 use App\Models\Customer;
+use App\Models\Order;
 use App\Models\OrderDetail;
 use App\Product;
 use App\Models\Review;
@@ -19,32 +22,196 @@ class CustomerController extends Controller
 {
     public function validateCoupon(Request $request){
         $coupon_discount = 0;
-        if ($request->coupon_code != '') {
-            $coupon  = Coupon::where('code', $request->coupon_code);
-            if($coupon->count() > 0){
-                $coupon = $coupon->first();
-                if(strtotime(date('d-m-Y')) <= $coupon->end_date){
-                    // $coupon_discount = $coupon->discount;
-                    return response()->json([
-                        'success' => true,
-                        'value' => $coupon->discount,
-                        'message' => 'Coupon Validated'
-                    ]);
-                }else{
+        $coupon = Coupon::where('code', $request->code)->first();
+        $cart = Cart::where('user_id', Auth::user()->id);
+        if($cart->count() > 0){
+            $cart = $cart->get()->toArray();
+        }else{            
+            return response()->json([
+                'success' => false,
+                'value' => 0,
+                'message' => 'There are no items in cart.'
+            ]);
+        }
+        if($coupon != null){
+            if($coupon->new_customer == 0){
+                if(strtotime(date('d-m-Y')) >= $coupon->start_date && strtotime(date('d-m-Y')) <= $coupon->end_date){
+                    if(CouponUsage::where('user_id', Auth::user()->id)->where('coupon_id', $coupon->id)->first() == null){
+                        $coupon_details = json_decode($coupon->details);
+                        if ($coupon->type == 'cart_base')
+                        {
+                            $subtotal = 0;
+                            $tax = 0;
+                            $shipping = 0;
+                            foreach ($cart as $key => $cartItem)
+                            {
+                                $subtotal += $cartItem['price']*$cartItem['quantity'];
+                                $tax += $cartItem['tax']*$cartItem['quantity'];
+                                $shipping += $cartItem['shipping_cost']*$cartItem['quantity'];
+                            }
+                            $sum = $subtotal+$tax+$shipping;
+
+                            if ($sum > $coupon_details->min_buy) {
+                                if ($coupon->discount_type == 'percent') {
+                                    $coupon_discount =  ($sum * $coupon->discount)/100;
+                                    if ($coupon_discount > $coupon_details->max_discount) {
+                                        $coupon_discount = $coupon_details->max_discount;
+                                    }
+                                }
+                                elseif ($coupon->discount_type == 'amount') {
+                                    $coupon_discount = $coupon->discount;
+                                }
+                                return response()->json([
+                                    'success' => true,
+                                    'value' => $coupon_discount,
+                                    'message' => 'Coupon Validated'
+                                ]);
+                            }else{
+                                return response()->json([
+                                    'success' => false,
+                                    'value' => 0,
+                                    'message' => 'Total buying should be greater than Rs.'.$coupon_details->min_buy.' to use this code.'
+                                ]);
+                            }
+                        }
+                        elseif ($coupon->type == 'product_base')
+                        {
+                            $coupon_discount = 0;
+                            $invalid = 0;
+                            foreach ($cart as $key => $cartItem){
+                                foreach ($coupon_details as $key => $coupon_detail) {
+                                    if($coupon_detail->product_id == $cartItem['id']){
+                                        if ($coupon->discount_type == 'percent') {
+                                            $coupon_discount += $cartItem['price']*$coupon->discount/100;
+                                        }
+                                        elseif ($coupon->discount_type == 'amount') {
+                                            $coupon_discount += $coupon->discount;
+                                        }
+                                        $invalid = 1;
+                                    }
+                                }
+                            }
+                            if($invalid == 0){
+                                return response()->json([
+                                    'success' => false,
+                                    'value' => 0,
+                                    'message' => 'Coupon invalid !'
+                                ]);
+                            }else{
+                                return response()->json([
+                                    'success' => true,
+                                    'value' => $coupon_discount,
+                                    'message' => 'Coupon valid !'
+                                ]);
+                            }
+                        }
+                    }
+                    else{
+                        return response()->json([
+                            'success' => false,
+                            'value' => 0,
+                            'message' => 'You already used this coupon!'
+                        ]);
+                    }
+                }
+                else{
                     return response()->json([
                         'success' => false,
                         'value' => 0,
-                        'message' => 'Coupon Code Expired'
+                        'message' => 'Coupon expired!'
                     ]);
                 }
             }else{
-                return response()->json([
-                    'success' => false,
-                    'value' => 0,
-                    'message' => 'Invalid Coupon Key'
-                ]);
-
+                $isNewUser = count(Order::where('user_id', Auth::user()->id)->get()) < 0 ? true : false;
+                if($isNewUser){
+                    if(strtotime(date('d-m-Y')) >= $coupon->start_date && strtotime(date('d-m-Y')) <= $coupon->end_date){
+                        if(CouponUsage::where('user_id', Auth::user()->id)->where('coupon_id', $coupon->id)->first() == null){
+                            $coupon_details = json_decode($coupon->details);
+    
+                            if ($coupon->type == 'cart_base')
+                            {
+                                $subtotal = 0;
+                                $tax = 0;
+                                $shipping = 0;
+                                foreach ($cart as $key => $cartItem)
+                                {
+                                    $subtotal += $cartItem['price']*$cartItem['quantity'];
+                                    $tax += $cartItem['tax']*$cartItem['quantity'];
+                                    $shipping += $cartItem['shipping_cost']*$cartItem['quantity'];
+                                }
+                                $sum = $subtotal+$tax+$shipping;
+    
+                                if ($sum > $coupon_details->min_buy) {
+                                    if ($coupon->discount_type == 'percent') {
+                                        $coupon_discount =  ($sum * $coupon->discount)/100;
+                                        if ($coupon_discount > $coupon_details->max_discount) {
+                                            $coupon_discount = $coupon_details->max_discount;
+                                        }
+                                    }
+                                    elseif ($coupon->discount_type == 'amount') {
+                                        $coupon_discount = $coupon->discount;
+                                    }
+                                    return response()->json([
+                                        'success' => true,
+                                        'value' => $coupon_discount,
+                                        'message' => 'Coupon valid!'
+                                    ]);
+                                }
+                            }
+                            elseif ($coupon->type == 'product_base')
+                            {
+                                $coupon_discount = 0;
+                                foreach ($cart as $key => $cartItem){
+                                    foreach ($coupon_details as $key => $coupon_detail) {
+                                        if($coupon_detail->product_id == $cartItem['id']){
+                                            if ($coupon->discount_type == 'percent') {
+                                                $coupon_discount += $cartItem['price']*$coupon->discount/100;
+                                            }
+                                            elseif ($coupon->discount_type == 'amount') {
+                                                $coupon_discount += $coupon->discount;
+                                            }
+                                        }
+                                    }
+                                }
+                                return response()->json([
+                                    'success' => true,
+                                    'value' => $coupon_discount,
+                                    'message' => 'Coupon valid!'
+                                ]);
+                            }
+                        }
+                        else{
+                            return response()->json([
+                                'success' => false,
+                                'value' => 0,
+                                'message' => 'You already used this coupon!'
+                            ]);
+                        }
+                    }
+                    else{
+                        return response()->json([
+                            'success' => false,
+                            'value' => 0,
+                            'message' => 'Coupon expired!'
+                        ]);
+                    }
+                }
+                else{
+                    return response()->json([
+                        'success' => false,
+                        'value' => 0,
+                        'message' => 'This Coupon Code is for new users!'
+                    ]);
+                }
             }
+
+        }
+        else {
+            return response()->json([
+                'success' => false,
+                'value' => 0,
+                'message' => 'Invalid coupon!'
+            ]);
         }
     }
     public function show($id)
